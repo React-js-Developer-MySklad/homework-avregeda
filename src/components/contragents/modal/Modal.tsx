e38973c3fcec6b9f97b75b680573d083f40aeb4d
aregeda <aregeda@moysklad.ru>
2024-07-03 18:57:57 +0300

    Реализовать валидацию данных формы в модальном окне с помощью final-form

--- src/components/contragents/modal/Modal.tsx
+++ src/components/contragents/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import './modal.css';
-import React, {FormEvent, useState} from "react";
+import React from "react";
 import {Contragent} from "../types";
+import {Field, Form} from "react-final-form";
 
 type iProps = {
     agent?: Contragent;
@@ -9,21 +10,16 @@ type iProps = {
 };
 
 export const Modal: React.FC<iProps> = ({agent, onClose, onSave}) => {
-    const [name, setName] = useState(agent ? agent.name : '');
-    const [inn, setInn] = useState(agent ? agent.inn : '');
-    const [kpp, setKpp] = useState(agent ? agent.kpp : '');
-    const [address, setAddress] = useState(agent ? agent.address : '');
 
-    const onSubmit = (e: FormEvent) => {
+    const onSubmit = (a: Contragent) => {
         const l = {
             id: agent?.id,
-            name: name,
-            inn: inn,
-            kpp: kpp,
-            address: address
+            name: a.name,
+            inn: a.inn,
+            kpp: a.kpp,
+            address: a.address
         };
         onSave(l);
-        e.preventDefault();
     }
 
     return (<>
@@ -38,37 +34,81 @@ export const Modal: React.FC<iProps> = ({agent, onClose, onSave}) => {
                                 <span>Закрыть</span>
                             </button>
                         </div>
-                        <form className="p-4 md:p-5" onSubmit={onSubmit}>
-                            <div className="grid gap-4 mb-4 grid-cols-2">
-                                <div className="col-span-2">
-                                    <label htmlFor="name" className="field-name">Наименование</label>
-                                    <input type="text" name="name" id="name" className="field-input" required
-                                           value={name} aria-label='name'
-                                           onChange={e => setName(e.target.value)}/>
-                                </div>
-                                <div className="col-span-2">
-                                    <label htmlFor="inn" className="field-name">ИНН</label>
-                                    <input type="number" name="inn" id="inn" className="field-input" required
-                                           value={inn} aria-label='inn'
-                                           onChange={e => setInn(e.target.value)}/>
-                                </div>
-                                <div className="col-span-2">
-                                    <label htmlFor="kpp" className="field-name">КПП</label>
-                                    <input type="number" name="kpp" id="kpp" className="field-input"
-                                           value={kpp} aria-label='kpp'
-                                           onChange={e => setKpp(e.target.value)}/>
-                                </div>
-                                <div className="col-span-2">
-                                    <label htmlFor="address" className="field-name">Адрес</label>
-                                    <textarea id="address" rows={4} className="field-area"
-                                              value={address} aria-label='address'
-                                              onChange={event => setAddress(event.target.value)}/>
-                                </div>
-                            </div>
-                            <button type="submit" className="add-button">
-                                Добавить
-                            </button>
-                        </form>
+                        <Form<Contragent> onSubmit={onSubmit} initialValues={agent} validateOnBlur>
+                            {props => (
+                                <form className="p-4 md:p-5" onSubmit={props.handleSubmit}>
+                                    <div className="grid gap-4 mb-4 grid-cols-2">
+                                        <div className="col-span-2">
+                                            <Field name='name'>
+                                                {props1 => (
+                                                    <>
+                                                        <label htmlFor="name"
+                                                               className="field-name">Наименование</label>
+                                                        <input type="text" name="name" id="name" required
+                                                               className="field-input" aria-label='name' {...props1.input}/>
+                                                        {props1.meta.error && <span>{props1.meta.error.message}</span>}
+                                                    </>
+                                                )}
+                                            </Field>
+                                        </div>
+                                        <div className="col-span-2">
+                                            <Field name='inn' validate={(value, allValues, meta) => {
+                                                if (value && value.length !== 12) {
+                                                    return {message: 'Длина ИНН должна быть 12 символов'}
+                                                }
+                                                return undefined;
+                                            }
+                                            }>
+                                                {props1 => (
+                                                    <>
+                                                        <label htmlFor="inn" className="field-name">ИНН</label>
+                                                        <input type="number" name="inn" id="inn" className="field-input"
+                                                               aria-label='inn'  {...props1.input}/>
+                                                        {props1.meta.error && <span>{props1.meta.error.message}</span>}
+                                                    </>
+                                                )}
+                                            </Field>
+                                        </div>
+                                        <div className="col-span-2">
+
+                                            <Field name='kpp' validate={(value, allValues, meta) => {
+                                                if (value && value.length !== 9) {
+                                                    return {message: 'Длина КПП должна быть 9 символов'}
+                                                }
+                                                return undefined;
+                                            }
+                                            }>
+                                                {props1 => (
+                                                    <>
+                                                        <label htmlFor="kpp" className="field-name">КПП</label>
+                                                        <input type="number" name="kpp" id="kpp" className="field-input"
+                                                               aria-label='kpp'  {...props1.input}/>
+                                                        {props1.meta.error && <span>{props1.meta.error.message}</span>}
+                                                    </>
+                                                )}
+                                            </Field>
+
+                                        </div>
+                                        <div className="col-span-2">
+                                            <Field name='address'>
+                                                {props1 => (
+                                                    <>
+                                                        <label htmlFor="address" className="field-name">Адрес</label>
+                                                        <textarea id="address" rows={4} className="field-area"
+                                                                  name='address'
+                                                                  aria-label='address'  {...props1.input}/>
+                                                    </>
+                                                )}
+                                            </Field>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="add-button">
+                                        Добавить
+                                    </button>
+                                </form>
+                            )
+                            }
+                        </Form>
                     </div>
                 </div>
             </div>
